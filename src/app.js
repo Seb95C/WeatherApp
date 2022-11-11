7b7ccfa4e32b47160b4a56266e90f7f048f24e2d
Sebastian Calcan <sebastian95calcan@gmail.com>
2022-11-11 18:11:03 +0200

    Simplified app.js. Added comments

--- src/app.js
+++ src/app.js
@@ -3,16 +3,20 @@ const weather = require('./utils/weather')
 
 geocode('Dubai', (error, data) => {
     if (error) { 
-        console.log('Error', error)
-    } else { 
-        console.log(data.label)
-
-        weather(data, (error, data) => {
-            if (error) { 
-                console.log('Error', error)
-            } else { 
-                console.log(data)
-            }
-        })
+        return console.log('Error', error)
     }
+    
+    // Display full location name
+    console.log(data.label)
+
+    // Call weather function with data (containing latitude, longitude)
+    // Label property is not used by weather. Kept like this for clarity of code
+    weather(data, (error, data) => {
+        if (error) { 
+            return console.log('Error', error)
+        }
+        
+        // Display response string containing description and temperature values
+        console.log(data)
+    })
 })
