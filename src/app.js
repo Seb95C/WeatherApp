041e67ef58a79b11a8a3917814391c13958ee1b3
Sebastian Calcan <sebastian95calcan@gmail.com>
2022-11-16 11:19:31 +0200

    Set up ExpressJS server for application

--- src/app.js
+++ src/app.js
@@ -1,30 +1,47 @@
 const geocode = require('./utils/geocode')
 const weather = require('./utils/weather')
 
-const location = process.argv[2]
+// Setting up Express server
+const express = require ('express')
+const app = express()
+const port = 3000
 
-// If no valid adress is provided, the application will terminate execution
-if (!location) {
-    return console.log('Please add a location when running the application via command line arguments.\nExample: node src/app.js London')
-}
-
-geocode(location, (error, data) => {
-    if (error) { 
-        return console.log('Error', error)
-    }
-    
-    // Call weather function with data (containing latitude, longitude)
-    // Label property is not used by weather. Kept like this for clarity of code
-    // weather() will destructure data to only use latitude and longitude
-    weather(data, (error, forecast) => {
-        if (error) { 
-            return console.log('Error', error)
-        }
-        
-        // Display full location name
-        console.log(data.label)
-
-        // Display response string containing description and temperature values
-        console.log(forecast)
-    })
+app.get('', (req, res) => {
+    res.send('Hello Express!')
 })
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
+})
+
+// Deprecated! Location will be received via HTTP request
+//const location = process.argv[2]
+
+// Deprecated! Location will be checked inside the handler
+// If no valid adress is provided, the application will terminate execution
+// if (!location) {
+//     return console.log('Please add a location when running the application via command line arguments.\nExample: node src/app.js London')
+// }
+
+
+// Code is deactivated temporarely for the purpose of developing the Express server
+// geocode(location, (error, data) => {
+//     if (error) { 
+//         return console.log('Error', error)
+//     }
+    
+//     // Call weather function with data (containing latitude, longitude)
+//     // Label property is not used by weather. Kept like this for clarity of code
+//     // weather() will destructure data to only use latitude and longitude
+//     weather(data, (error, forecast) => {
+//         if (error) { 
+//             return console.log('Error', error)
+//         }
+        
+//         // Display full location name
+//         console.log(data.label)
+
+//         // Display response string containing description and temperature values
+//         console.log(forecast)
+//     })
+// })
