2f7cc160a442cafe14da2d89ae4e6d0eefc58944
Sebastian Calcan <sebastian95calcan@gmail.com>
2022-11-17 13:39:23 +0200

    Response is now displayed on the page

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,23 @@
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const message1 = document.querySelector('#msg1') 
+const message2 = document.querySelector('#msg2')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
 
+    message1.textContent = 'Loading...'
+    message2.textContent = ''
+
     fetch(`/weather?adress=${encodeURIComponent(location)}`).then( (response) => {
         response.json().then((data) => {
             if(data.error) {
-                console.log(data.error)
+                message1.textContent = data.error
             } else {
-                console.log(data.location)
-                console.log(data.forecast)
+                message1.textContent = data.location
+                message2.textContent = data.forecast
             }
         })
     })
